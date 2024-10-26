c0a35d7b507d404a5d3e626a757900bc0e05fd62
hou-jch <1724048749@qq.com>
2024-10-26 20:00:02 +0800

    api接口项目,网关项目-测试提交

--- src/main/java/com/hjc/houapigateway/CustomGlobalFilter.java
+++ src/main/java/com/hjc/houapigateway/CustomGlobalFilter.java
@@ -1,22 +1,29 @@
 package com.hjc.houapigateway;
 
+import com.hjc.hjjcclientsdk.util.SignUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import com.hjc.hjjcclientsdk.util.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static com.hjc.hjjcclientsdk.util.SignUtils.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 /**
@@ -57,8 +64,9 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         String body = headers.getFirst("body");
 
 // todo 实际情况应该是去数据库中查是否已分配给用户
-        if (!"yupi".equals(accessKey)){
+        if (!"5f3fcad2098c9adfbd74cd2f6e24250f".equals(accessKey)){
             return handleNoAuth(response);
+
         }
 // 直接校验如果随机数大于1万，则抛出异常，并提示"无权限"
         if (Long.parseLong(nonce) > 10000) {
@@ -73,30 +81,21 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
 
 
 // todo 实际情况中是从数据库中查出 secretKey
-        String serverSign = getSion(body, "abcdefgh");
+        String serverSign = SignUtils.getSion(body, "6131b2e69fdf5f5715ed7a43461a6727");
 // 如果生成的签名不一致，则抛出异常，并提示"无权限"
         if (!sign.equals(serverSign)) {
             return handleNoAuth(response);
         }
 // todo 调用次数 + 1
 
-
+//        return handleNoAuth(response);
         //5.请求的模拟接口是杏存在?
         //6.请求转发，调用模拟接口
-        Mono<Void> filter = chain.filter(exchange);
-
-        //7.响应日志
-        log.info("响应:" + response.getStatusCode());
-        //8.调用成功，接口调用次数+1
-        if(response.getStatusCode() == HttpStatus.OK){
-
-        }else{
-            return handleInvokeError(response);
-        }
-        //9.调用失败，返回一个规范的错误码
+        return handleResponse(exchange, chain);
 
 
-        return filter;
+
+
     }
 
     @Override
@@ -126,5 +125,94 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
         return response.setComplete();
     }
+
+
+    /**
+     * 处理响应
+     *
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
+            // 获取原始的响应对象
+            ServerHttpResponse originalResponse = exchange.getResponse();
+            // 获取数据缓冲工厂
+            DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+            // 获取响应的状态码
+            HttpStatus statusCode = originalResponse.getStatusCode();
+
+            // 判断状态码是否为200 OK(按道理来说,现在没有调用,是拿不到响应码的,对这个保持怀疑 沉思.jpg)
+            if(statusCode == HttpStatus.OK) {
+                // 创建一个装饰后的响应对象(开始穿装备，增强能力)
+                ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+
+                    // 重写writeWith方法，用于处理响应体的数据
+                    // 这段方法就是只要当我们的模拟接口调用完成之后,等它返回结果，
+                    // 就会调用writeWith方法,我们就能根据响应结果做一些自己的处理
+                    @Override
+                    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                        log.info("body instanceof Flux: {}", (body instanceof Flux));
+
+                        // 判断响应体是否是Flux类型
+                        if (body instanceof Flux) {
+                            Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                            // 返回一个处理后的响应体
+                            // (这里就理解为它在拼接字符串,它把缓冲区的数据取出来，一点一点拼接好)
+                            //8.调用成功，接口调用次数+1
+                            if(originalResponse.getStatusCode() == HttpStatus.OK){  //调用过程中无异常
+
+                                System.out.println("调用成功，接口调用次数+1");
+                            }else{
+                                System.out.println("调用失败");
+                                log.error("<--- {} 响应code异常", getStatusCode()); //记录异常日志
+
+                            }
+//                            AtomicReference<String> responseData = new AtomicReference<>();
+                            Mono<Void> voidMono = super.writeWith(
+                                    fluxBody.map(dataBuffer -> {
+                                        // 读取响应体的内容并转换为字节数组
+                                        byte[] content = new byte[dataBuffer.readableByteCount()];
+                                        dataBuffer.read(content);
+                                        DataBufferUtils.release(dataBuffer);//释放掉内存
+                                        // 构建日志
+                                        StringBuilder sb2 = new StringBuilder(200);
+                                        List<Object> rspArgs = new ArrayList<>();
+                                        rspArgs.add(originalResponse.getStatusCode());
+                                        String data = new String(content, StandardCharsets.UTF_8);//data
+                                        sb2.append(data);
+                                        //7.响应日志
+//                                        responseData.set(data);
+                                        log.info("响应:" + data);
+                                        // 将处理后的内容重新包装成DataBuffer并返回
+
+                                        return bufferFactory.wrap(content);
+                                    }));
+//                            log.info(responseData.get());
+                            return voidMono;
+
+                        } else {
+                            //9.调用失败，返回一个规范的错误码
+
+                            log.error("<--- {} 响应code异常", getStatusCode()); //记录异常日志
+                        }
+                        return super.writeWith(body);
+                    }
+                };
+                // 对于200 OK的请求,将装饰后的响应对象传递给下一个过滤器链,并继续处理(设置repsonse对象为装饰过的)
+
+                return chain.filter(exchange.mutate().response(decoratedResponse).build());
+            }
+            // 对于非200 OK的请求，直接返回，进行降级处理
+//            exchange.getResponse().setStatusCode(HttpStatus.BAD_GATEWAY);
+            return chain.filter(exchange);
+        }catch (Exception e){
+            // 处理异常情况，记录错误日志
+            log.error("网关处理响应异常" + e);
+            return chain.filter(exchange);
+        }
+    }
+
 }
 
